3c129a2c82d78d4ccb5e5318f6f286f3c411bc5d
Vjfrib <vjfr@ecomp.poli.br>
2023-05-16 18:05:30 +0000

    variavel

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 //Estudo Procedural de Java
+//link do canal que acompanho
+//https://www.youtube.com/watch?v=EP7qRe5FjPY&list=PLGPluF_nhP9p6zWTN88ZJ1q9J_ZK148-f&index=3
 
 class Main {
   public static void main(String[] args) {
@@ -6,15 +8,16 @@ class Main {
     System.out.println("\nPq tudo em java eh taum longo?");
     //com o git checkout, consegui fazer a volta para aqui.
 
-    /*o replit é bom para aprender a ling, pois te obriga a usar o bash e ainda oferece suporte pra linga, puramente.
+    /*o replit é bom para aprender a ling, pois te obriga a usar o bash e ainda oferece suporte pra lang, puramente. Lembrando que o github tbm faz isso.
     
     GIT - CONF E COMMAND
     git config --global user.email "you@example.com"
     git config --global user.name "Your Name"
-  
+    git push origin HEAD:refs/heads/<nova branch, caso precise>
+
     lembrando que temos:
     git add .                                  <o ponto serve pra add tudo>
-    git commit
+    git commit                              -m <comment> ou :wq (write and quit)
     git checkout <hash>
     git checkout -b <brach nova>
     git checkout -b <brach nova> <hash>
@@ -26,5 +29,13 @@ class Main {
 
     //input - sys.out
     System.out.println("\n Teste.");
+    //out puts-
+
+    //variáveis primitivas - Simples, valor fixo
+    byte numPequeno = 124;
+    
+  
+
+    
   }
 }
