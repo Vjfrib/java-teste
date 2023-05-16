61c9baa1ff0f83f504aba30ec9ad0fbee048e8b9
Vjfrib <vjfr@ecomp.poli.br>
2023-05-16 17:45:42 +0000

    mensagem padrão do commit
    abaixo: especificações-->

--- Main.java
+++ Main.java
@@ -4,5 +4,27 @@ class Main {
   public static void main(String[] args) {
     System.out.println("\nprimeiro print");
     System.out.println("\nPq tudo em java eh taum longo?");
+    //com o git checkout, consegui fazer a volta para aqui.
+
+    /*o replit é bom para aprender a ling, pois te obriga a usar o bash e ainda oferece suporte pra linga, puramente.
+    
+    GIT - CONF E COMMAND
+    git config --global user.email "you@example.com"
+    git config --global user.name "Your Name"
+  
+    lembrando que temos:
+    git add .                                  <o ponto serve pra add tudo>
+    git commit
+    git checkout <hash>
+    git checkout -b <brach nova>
+    git checkout -b <brach nova> <hash>
+    git push
+    git pull
+    */
+
+    //vamos lá!!
+
+    //input - sys.out
+    System.out.println("\n Teste.");
   }
 }
---
+++ readme.md
@@ -0,0 +1,7 @@
+Projeto escrito em java para testa funcionalidades
+
+-Imperativas
+-OOP
+-Funcional
+-Graph & games
+-Web e Pentest
